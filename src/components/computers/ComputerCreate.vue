211e2439fa2ddbf570a3d4271c86e18aa6d0c3af
Phong Tran <tranphongbb@icloud.com>
2020-05-23 13:03:27 +0700

    clean up and add order & computer create component

---
+++ src/components/computers/ComputerCreate.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <b-form-group label="Mainboard:">
+            <b-form-select required></b-form-select>
+        </b-form-group>
+        <b-form-group label="CPU:">
+            <b-form-select required></b-form-select>
+        </b-form-group>
+        <b-form-group label="RAM:">
+            <div style="display: flex">
+                <b-form-select required style="width: 80%"></b-form-select>
+                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
+                              style="width: 20%" type="number"></b-form-input>
+            </div>
+        </b-form-group>
+        <b-form-group label="GPU:">
+            <div style="display: flex">
+                <b-form-select required style="width: 80%"></b-form-select>
+                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
+                              style="width: 20%" type="number"></b-form-input>
+            </div>
+        </b-form-group>
+        <b-form-group label="Nguồn:">
+            <div style="display: flex">
+                <b-form-select required style="width: 80%"></b-form-select>
+                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
+                              style="width: 20%" type="number"></b-form-input>
+            </div>
+        </b-form-group>
+        <b-form-group label="SSD:">
+            <div style="display: flex">
+                <b-form-select required style="width: 80%"></b-form-select>
+                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
+                              style="width: 20%" type="number"></b-form-input>
+            </div>
+        </b-form-group>
+        <b-form-group label="Nguồn:">
+            <div style="display: flex">
+                <b-form-select required style="width: 80%"></b-form-select>
+                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
+                              style="width: 20%" type="number"></b-form-input>
+            </div>
+        </b-form-group>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ComputerCreate"
+    }
+</script>
+
+<style scoped>
+
+</style>
