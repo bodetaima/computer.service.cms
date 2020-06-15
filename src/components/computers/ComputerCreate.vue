6830d5aa22b63990490507dce429e3430abd4bc6
Phong Tran <tranphongbb@icloud.com>
2020-06-16 00:18:56 +0700

    minor stage

--- src/components/computers/ComputerCreate.vue
+++ src/components/computers/ComputerCreate.vue
@@ -1,55 +1,61 @@
 <template>
-    <div>
-        <b-form-group label="Mainboard:">
-            <b-form-select required></b-form-select>
-        </b-form-group>
-        <b-form-group label="CPU:">
-            <b-form-select required></b-form-select>
-        </b-form-group>
-        <b-form-group label="RAM:">
-            <div style="display: flex">
-                <b-form-select required style="width: 80%"></b-form-select>
-                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
-                              style="width: 20%" type="number"></b-form-input>
-            </div>
-        </b-form-group>
-        <b-form-group label="GPU:">
-            <div style="display: flex">
-                <b-form-select required style="width: 80%"></b-form-select>
-                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
-                              style="width: 20%" type="number"></b-form-input>
-            </div>
-        </b-form-group>
-        <b-form-group label="Nguồn:">
-            <div style="display: flex">
-                <b-form-select required style="width: 80%"></b-form-select>
-                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
-                              style="width: 20%" type="number"></b-form-input>
-            </div>
-        </b-form-group>
-        <b-form-group label="SSD:">
-            <div style="display: flex">
-                <b-form-select required style="width: 80%"></b-form-select>
-                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
-                              style="width: 20%" type="number"></b-form-input>
-            </div>
-        </b-form-group>
-        <b-form-group label="Nguồn:">
-            <div style="display: flex">
-                <b-form-select required style="width: 80%"></b-form-select>
-                <b-form-input min="0" oninput="this.value = Math.abs(this.value)" placeholder="Số lượng" required
-                              style="width: 20%" type="number"></b-form-input>
-            </div>
-        </b-form-group>
-    </div>
+  <div>
+    <b-button v-b-modal.mb block>Mainboard</b-button>
+    <b-modal size="xl" id="mb" title="Mainboard">
+      <table class="table table-stripped">
+          <thead>
+              <tr>
+                  <th>Tên linh kiện</th>
+                  <th>Giá linh kiện</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(part, index) in parts" :key="index">
+                  <td>{{ part.name }}</td>
+                  <td>{{ part.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND'}) }}</td>
+              </tr>
+          </tbody>
+      </table>
+    </b-modal>
+    <b-button v-b-modal.cpu block>CPU</b-button>
+    <b-modal id="cpu" title="CPU">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-button v-b-modal.ram block>RAM</b-button>
+    <b-modal id="ram" title="RAM">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-button v-b-modal.gpu block>GPU</b-button>
+    <b-modal id="gpu" title="GPU">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-button v-b-modal.psu block>Nguồn</b-button>
+    <b-modal id="psu" title="Nguồn">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-button v-b-modal.ssd block>SSD</b-button>
+    <b-modal id="ssd" title="SSD">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-button v-b-modal.hdd block>HDD</b-button>
+    <b-modal id="hdd" title="HDD">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "ComputerCreate"
-    }
+export default {
+  name: "ComputerCreate",
+  props: {
+      parts: Array,
+      size: Number,
+      page: Number,
+      totalPages: Number,
+      totalElements: Number
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
