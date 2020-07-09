093dc55986b8f4e578e40ebcb64e1bde6df27df3
Phong Tran <tranphongbb@icloud.com>
2020-07-09 17:50:05 +0700

    minor fixes

--- src/services/request.service.js
+++ src/services/request.service.js
@@ -1,2 +1,4 @@
-export const API_URL = "http://localhost:1025/api/";
-export const AUTH_URL = 'http://localhost:1025/api/auth/';
+export const URL = "https://computer-services.herokuapp.com/";
+// export const URL = "http://localhost:1025/";
+export const API_URL = URL + "api/";
+export const AUTH_URL = AUTH_URL + "auth/";
